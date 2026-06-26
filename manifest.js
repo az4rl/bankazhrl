4e9f3f10c0276404d9c921909aff71f8f039f2a2
azrl <117619738+az4rl@users.noreply.github.com>
2026-06-26 14:26:16 +0700

    192512

--- manifest.js
+++ manifest.js
@@ -9,13 +9,13 @@
   "theme_color": "#111827",
   "icons": [
     {
-      "src": "icon.png",
+      "src": "192.png",
       "sizes": "192x192",
       "type": "image/svg+xml",
       "purpose": "any maskable"
     },
     {
-      "src": "icon.png",
+      "src": "512.png",
       "sizes": "512x512",
       "type": "image/svg+xml",
       "purpose": "any maskable"
